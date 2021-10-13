0302711aed9e4aed3c31aaf9bf274efdcfda0739
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-10-13 13:19:13 -0600

    started mvp

--- index.js
+++ index.js
@@ -42,7 +42,24 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  }
+  eat(someFood){
+    if(this.stomach.length < 10){
+       this.stomach.push(someFood);
+    }
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -60,7 +77,28 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0
+}
+
+fill(gallons){
+this.tank += gallons
+}
+
+drive(distance){
+if(this.tank * this.milesPerGallon < distance){
+  this.odometer = this.tank * this.milesPerGallon;
+  this.tank = 0;
+  return `I ran out of fuel at ${this.odometer} miles!`
+}
+  else{
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - distance/this.milesPerGallon;
+  }
+}
 }
 
 /*
